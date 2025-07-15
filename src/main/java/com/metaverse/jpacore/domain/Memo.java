da7548f5c20be521357561cf02578374cf009aec
jej031020 <jej031020@naver.com>
2025-07-15 15:33:03 +0900

    test: JPA - 영속성과 트랜잭션 관련 테스트

--- src/main/java/com/metaverse/jpacore/domain/Memo.java
+++ src/main/java/com/metaverse/jpacore/domain/Memo.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 // @Table(name = "memo") // 맵핑할 테이블 이름을 지정(생략 시, 클래스명 기본값)
 public class Memo {
     @Id // PK 설정
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성 DB에 위임
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성 DB에 위임
     private Long id;
 
     @Column(length = 100, nullable = false, unique = true)
@@ -14,5 +14,16 @@ public class Memo {
 
     @Column(length = 500)
     private String contents;
-}
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
--- src/test/java/com/metaverse/jpacore/EntityTest.java
+++ src/test/java/com/metaverse/jpacore/EntityTest.java
@@ -1,9 +1,12 @@
 package com.metaverse.jpacore;
 
+import com.metaverse.jpacore.domain.Memo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EntityTest {
@@ -17,7 +20,48 @@ public class EntityTest {
     }
 
     @Test
-    void test1() {
+    @DisplayName("Entity Transaction 성공 테스트")
+    void test_transaction_1() {
+        EntityTransaction et = em.getTransaction();
+        et.begin();
 
+        try {
+            Memo memo = new Memo(); // 저장할 엔터티 객체 생성
+            memo.setId(1L);
+            memo.setUsername("Meta");
+            memo.setContents("영속성 컨텍스트와 트랜잭션 이해하기");
+
+            em.persist(memo); // EntityManager가 memo 객체를 영속성 컨텍스트에 저장한다.
+
+            et.commit(); // 오류가 없으면 정상수행, 커밋 호출
+        } catch (Exception e) {
+            e.printStackTrace();
+            et.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("EntityTransaction 실패 테스트")
+    void test_transaction_2() {
+        EntityTransaction et = em.getTransaction();
+        et.begin(); // 트랜잭션 실행
+
+        try {
+            Memo memo = new Memo();
+            memo.setUsername("Meta");
+            memo.setContents("Memo입니다");
+
+            em.persist(memo);
+            et.commit();
+        } catch (Exception e) {
+            System.out.println("식별자 값(id)를 입력하지 않아 오류가 발생했음");
+            e.printStackTrace();
+            et.rollback();
+        } finally {
+            emf.close();
+        }
     }
 }
